8e7c0a0d1f0438c299ffd131dafad96c17b712c0
Ihor <ihor@hivepress.io>
2020-05-13 23:33:53 +0300

    Add geolocation settings

--- includes/configs/settings.php
+++ includes/configs/settings.php
@@ -11,11 +11,42 @@ use HivePress\Helpers as hp;
 defined( 'ABSPATH' ) || exit;
 
 return [
+	'geolocation'  => [
+		'title'    => esc_html__( 'Geolocation', 'hivepress-geolocation' ),
+		'_order'   => 15,
+
+		'sections' => [
+			'search' => [
+				'title'  => hivepress()->translator->get_string( 'search_noun' ),
+				'_order' => 10,
+
+				'fields' => [
+					'geolocation_radius'    => [
+						'label'     => esc_html__( 'Radius', 'hivepress-geolocation' ),
+						'type'      => 'number',
+						'min_value' => 1,
+						'default'   => 15,
+						'required'  => true,
+						'_order'    => 10,
+					],
+
+					'geolocation_countries' => [
+						'label'    => esc_html__( 'Countries', 'hivepress-geolocation' ),
+						'type'     => 'select',
+						'options'  => 'countries',
+						'multiple' => true,
+						'_order'   => 20,
+					],
+				],
+			],
+		],
+	],
+
 	'integrations' => [
 		'sections' => [
 			'gmaps' => [
 				'title'  => 'Google Maps',
-				'_order' => 20,
+				'_order' => 30,
 
 				'fields' => [
 					'gmaps_api_key' => [
