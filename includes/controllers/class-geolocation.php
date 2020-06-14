8664ee99ffd3148f1a06d57f8a49b4578c60a26d
Ihor <ihor@hivepress.io>
2020-06-14 18:55:47 +0300

    Make locations clickable

---
+++ includes/controllers/class-geolocation.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Geolocation controller.
+ *
+ * @package HivePress\Controllers
+ */
+
+namespace HivePress\Controllers;
+
+use HivePress\Helpers as hp;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Geolocation controller class.
+ *
+ * @class Geolocation
+ */
+final class Geolocation extends Controller {
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $args Controller arguments.
+	 */
+	public function __construct( $args = [] ) {
+		$args = hp\merge_arrays(
+			[
+				'routes' => [
+					'location_view_page' => [
+						'url' => [ $this, 'get_location_view_url' ],
+					],
+				],
+			],
+			$args
+		);
+
+		parent::__construct( $args );
+	}
+
+	/**
+	 * Gets location view URL.
+	 *
+	 * @param array $params URL parameters.
+	 * @return string
+	 */
+	public function get_location_view_url( $params ) {
+		return add_query_arg(
+			[
+				'api'   => 1,
+				'query' => hp\get_array_value( $params, 'latitude' ) . ',' . hp\get_array_value( $params, 'longitude' ),
+			],
+			'https://www.google.com/maps/search/'
+		);
+	}
+}
--- templates/listing/view/listing-location.php
+++ templates/listing/view/listing-location.php
@@ -4,6 +4,9 @@ defined( 'ABSPATH' ) || exit;
 
 if ( $listing->get_location() ) :
 	?>
-	<div class="hp-listing__location"><i class="hp-icon fas fa-map-marker-alt"></i><span><?php echo esc_html( $listing->get_location() ); ?></span></div>
+	<div class="hp-listing__location">
+		<i class="hp-icon fas fa-map-marker-alt"></i>
+		<a href="<?php echo esc_url( hivepress()->router->get_url( 'location_view_page', [ 'latitude' => $listing->get_latitude(), 'longitude' => $listing->get_longitude() ] ) ); ?>" target="_blank"><?php echo esc_html( $listing->get_location() ); ?></a>
+	</div>
 	<?php
 endif;
