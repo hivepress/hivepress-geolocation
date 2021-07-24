a8b7991d9f1f19d6ae954c7a0f7a55f8e0ff0781
Ihor <ihor@hivepress.io>
2021-07-25 01:00:56 +0300

    Update version

--- hivepress-geolocation.php
+++ hivepress-geolocation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HivePress Geolocation
  * Description: Allow users to search listings by location.
- * Version: 1.2.5
+ * Version: 1.2.6
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress-geolocation
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: hivepress, geolocation, directory, listings, classifieds
 Requires at least: 4.9
 Tested up to: 5.8
 Requires PHP: 5.6
-Stable tag: 1.2.5
+Stable tag: 1.2.6
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
