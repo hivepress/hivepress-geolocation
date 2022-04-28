882327afdb0e08d08c48296cbbfacb50a4ca9a50
CoolAppCreation <kuznietevhen@gmail.com>
2022-04-28 18:04:58 +0300

    Add strings prepared for translation

--- includes/configs/strings.php
+++ includes/configs/strings.php
@@ -13,12 +13,16 @@ defined( 'ABSPATH' ) || exit;
 return [
 
 	// Common.
-	'km'      => esc_html__( 'km', 'hivepress-geolocation' ),
-	'miles'   => esc_html__( 'miles', 'hivepress-geolocation' ),
+	'km'             => esc_html__( 'km', 'hivepress-geolocation' ),
+	'miles'          => esc_html__( 'miles', 'hivepress-geolocation' ),
 
 	// Geographical areas.
-	'country' => esc_html__( 'Country', 'hivepress-geolocation' ),
-	'state'   => esc_html__( 'State', 'hivepress-geolocation' ),
-	'county'  => esc_html__( 'County', 'hivepress-geolocation' ),
-	'city'    => esc_html__( 'City', 'hivepress-geolocation' ),
+	'country'        => esc_html__( 'Country', 'hivepress-geolocation' ),
+	'state'          => esc_html__( 'State', 'hivepress-geolocation' ),
+	'county'         => esc_html__( 'County', 'hivepress-geolocation' ),
+	'city'           => esc_html__( 'City', 'hivepress-geolocation' ),
+
+	// API.
+	'public_api_key' => esc_html__( 'Public API Key', 'hivepress-geolocation' ),
+	'secret_api_key' => esc_html__( 'Secret API Key', 'hivepress-geolocation' ),
 ];
