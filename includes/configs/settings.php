1a96d2298baf64dde4ffccd5ddffbd20a68b7fdf
ihor <ihor@hivepress.io>
2022-06-03 22:49:12 +0300

    Add model selection

--- includes/configs/settings.php
+++ includes/configs/settings.php
@@ -21,11 +21,26 @@ return [
 				'_order' => 100,
 
 				'fields' => [
+					'geolocation_models'           => [
+						'label'       => esc_html__( 'todo', 'hivepress-geolocation' ),
+						'description' => esc_html__( 'todo', 'hivepress-geolocation' ),
+						'type'        => 'select',
+						'default'     => [ 'listing' ],
+						'multiple'    => true,
+						'required'    => true,
+						'_order'      => 10,
+
+						'options'     => [
+							'listing' => hivepress()->translator->get_string( 'listings' ),
+							'vendor'  => hivepress()->translator->get_string( 'vendors' ),
+						],
+					],
+
 					'geolocation_provider'         => [
 						'label'       => esc_html__( 'Map Provider', 'hivepress-geolocation' ),
 						'type'        => 'select',
 						'placeholder' => 'Google Maps',
-						'_order'      => 10,
+						'_order'      => 20,
 
 						'options'     => [
 							'mapbox' => 'Mapbox',
@@ -37,7 +52,7 @@ return [
 						'type'     => 'select',
 						'options'  => 'countries',
 						'multiple' => true,
-						'_order'   => 20,
+						'_order'   => 30,
 					],
 
 					'geolocation_max_zoom'         => [
@@ -47,7 +62,7 @@ return [
 						'max_value' => 20,
 						'default'   => 18,
 						'required'  => true,
-						'_order'    => 30,
+						'_order'    => 40,
 					],
 
 					'geolocation_radius'           => [
@@ -57,19 +72,19 @@ return [
 						'min_value'   => 1,
 						'default'     => 15,
 						'required'    => true,
-						'_order'      => 40,
+						'_order'      => 50,
 					],
 
 					'geolocation_use_miles'        => [
 						'caption' => esc_html__( 'Use miles instead of kilometers', 'hivepress-geolocation' ),
 						'type'    => 'checkbox',
-						'_order'  => 50,
+						'_order'  => 60,
 					],
 
 					'geolocation_allow_radius'     => [
 						'caption' => esc_html__( 'Allow users to change radius', 'hivepress-geolocation' ),
 						'type'    => 'checkbox',
-						'_order'  => 60,
+						'_order'  => 70,
 					],
 
 					'geolocation_generate_regions' => [
@@ -77,7 +92,7 @@ return [
 						'description' => esc_html__( 'Check this option to create a page for each region.', 'hivepress-geolocation' ),
 						'caption'     => esc_html__( 'Generate regions from locations', 'hivepress-geolocation' ),
 						'type'        => 'checkbox',
-						'_order'      => 70,
+						'_order'      => 80,
 					],
 
 					'geolocation_region_levels'    => [
@@ -86,7 +101,7 @@ return [
 						'type'        => 'select',
 						'multiple'    => true,
 						'_parent'     => 'geolocation_generate_regions',
-						'_order'      => 80,
+						'_order'      => 90,
 
 						'options'     => [
 							'country' => esc_html__( 'Country', 'hivepress-geolocation' ),
@@ -101,14 +116,14 @@ return [
 						'description' => esc_html__( 'todo', 'hivepress-geolocation' ) . ' ' . sprintf( hivepress()->translator->get_string( 'these_tokens_are_available' ), '%listing%, %vendor%' ),
 						'type'        => 'text',
 						'max_length'  => 256,
-						'_order'      => 90,
+						'_order'      => 100,
 					],
 
 					'geolocation_hide_address'     => [
 						'description' => esc_html__( 'todo.', 'hivepress-geolocation' ),
 						'caption'     => esc_html__( 'Hide the exact address', 'hivepress-geolocation' ),
 						'type'        => 'checkbox',
-						'_order'      => 100,
+						'_order'      => 110,
 					],
 				],
 			],
