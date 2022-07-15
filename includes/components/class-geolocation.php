23783aab989f0e3ae3483a5c9371cc9460c99626
kuznev <kuznietevhen@gmail.com>
2022-07-15 13:55:48 +0300

    Minor fixes

--- includes/components/class-geolocation.php
+++ includes/components/class-geolocation.php
@@ -241,7 +241,6 @@ final class Geolocation extends Component {
 				'https://maps.googleapis.com/maps/api/js?' . http_build_query(
 					[
 						'libraries' => 'places',
-						'callback'  => 'hivepress.initGeolocation',
 						'key'       => get_option( 'hp_gmaps_api_key' ),
 						'language'  => hivepress()->translator->get_language(),
 						'region'    => hivepress()->translator->get_region(),
