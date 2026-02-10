eee629e7c8ab6885b886c9d0cdacfb0b51bc65d7
HivePress <43493187+hivepress@users.noreply.github.com>
2026-02-10 22:49:35 +0000

    Revert "Allow disabling the location search"
    This reverts commit 7c51517811f21e12c255d7407df6647867fe34c4.

--- includes/components/class-geolocation.php
+++ includes/components/class-geolocation.php
@@ -127,9 +127,6 @@ final class Geolocation extends Component {
 		// Get model.
 		$model = explode( '/', current_filter() )[3];
 
-		// Get searchability.
-		$searchable = get_option( 'hp_installed_time' ) < strtotime( '2026-02-11' ) || in_array( 'location', (array) get_option( hp\prefix( $model . '_search_fields' ), [ 'location' ] ) );
-
 		// Get countries.
 		$countries = array_filter( (array) get_option( 'hp_geolocation_countries' ) );
 
@@ -146,7 +143,7 @@ final class Geolocation extends Component {
 			[
 				'location'  => [
 					'editable'     => true,
-					'searchable'   => $searchable,
+					'searchable'   => true,
 
 					'edit_field'   => [
 						'label'     => esc_html__( 'Location', 'hivepress-geolocation' ),
@@ -166,7 +163,7 @@ final class Geolocation extends Component {
 
 				'latitude'  => [
 					'editable'     => true,
-					'searchable'   => $searchable,
+					'searchable'   => true,
 
 					'edit_field'   => [
 						'label' => esc_html__( 'Latitude', 'hivepress-geolocation' ),
@@ -181,7 +178,7 @@ final class Geolocation extends Component {
 
 				'longitude' => [
 					'editable'     => true,
-					'searchable'   => $searchable,
+					'searchable'   => true,
 
 					'edit_field'   => [
 						'label' => esc_html__( 'Longitude', 'hivepress-geolocation' ),
@@ -211,7 +208,7 @@ final class Geolocation extends Component {
 
 			$attributes['region'] = [
 				'protected'    => true,
-				'filterable'   => $searchable,
+				'filterable'   => true,
 
 				'edit_field'   => $field_args,
 				'search_field' => $field_args,
@@ -496,16 +493,6 @@ final class Geolocation extends Component {
 
 		if ( get_option( 'hp_installed_time' ) < strtotime( '2025-10-05' ) ) {
 			$settings['integrations']['sections']['gmaps']['fields']['gmaps_use_legacy_api']['default'] = true;
-		}
-
-		if ( get_option( 'hp_installed_time' ) > strtotime( '2026-02-11' ) ) {
-			foreach ( $this->models as $model ) {
-				if ( isset( $settings[ $model . 's' ]['sections']['search']['fields'][ $model . '_search_fields' ] ) ) {
-					$settings[ $model . 's' ]['sections']['search']['fields'][ $model . '_search_fields' ]['options']['location'] = esc_html__( 'Location', 'hivepress-geolocation' );
-
-					$settings[ $model . 's' ]['sections']['search']['fields'][ $model . '_search_fields' ]['default'][] = 'location';
-				}
-			}
 		}
 
 		if ( isset( $settings['listings']['sections']['display']['fields']['listing_related_criteria'] ) ) {
